e91ad4f2d304e45380b08440ffb91d1da99c982c
Felice Forby <feliceforby@gmail.com>
2018-10-28 23:43:57 +0000

    add boat class methods and stub out captain class methods

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,16 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+  end
+
+  def self.sailors
+  å
+  end
+
+  def self.talented_seafarers
+  end
+
+  def self.non_sailors
+  end
 end
