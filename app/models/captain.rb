e83fca82fb60a0614ade2787c615920aa34ce135
Felice Forby <feliceforby@gmail.com>
2018-10-29 22:18:43 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    joins(boats: [:classifications]).where('classifications.name = ? AND classifications.name = _)', 'Sailboat', 'Motorboat').distinct
+    joins(boats: [:classifications]).where('classifications.name = ? AND classifications.name = ?)', 'Sailboat', 'Motorboat').distinct
   end
 
   def self.non_sailors
