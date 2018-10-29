c9bb7fcc377dea3de9670466db19c95f438bd7dd
Felice Forby <feliceforby@gmail.com>
2018-10-29 04:28:26 +0000

    continue captain class methods

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,13 +2,15 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
+    joins(boats: [:classifications]).where('classifications.name = ?', 'Catamaran').distinct
   end
 
   def self.sailors
-  å
+    joins(boats: [:classifications]).where('classifications.name = ?', 'Sailboat').distinct
   end
 
   def self.talented_seafarers
+    joins(boats: [:classifications]).where(name: ['Sailboat', 'Motorboat'])
   end
 
   def self.non_sailors
