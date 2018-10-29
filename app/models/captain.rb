679caca1832d81632485cd21c3f264baf1cebd9d
Felice Forby <feliceforby@gmail.com>
2018-10-29 23:15:11 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -31,6 +31,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.non_sailors
-    joins(boats: [:classifications]).where.not(classifications: {name: 'Sailboat'})
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 end
