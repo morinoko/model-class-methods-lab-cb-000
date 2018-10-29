b75f1271d0caa508b28c89793f4de82988ccf2e8
Felice Forby <feliceforby@gmail.com>
2018-10-29 22:20:50 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,12 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
-  
+
   def self.my_all
     all
   end
-  
+
   def self.longest
-    joins(:boats).order('boat.length' :desc).limit(1)
+    joins(:boats).order('boat.length' => :desc).limit(1)
   end
 end
